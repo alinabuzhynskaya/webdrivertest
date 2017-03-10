a67b68869a741edc808dbf3667e499e7c9e98e0e
Alina Buzhynskaya <alinabuzhynskaya@gmail.com>
2017-03-10 23:54:03 +0300

    added cases for user1 and user2

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -5,7 +5,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Unit test for simple App.
@@ -13,7 +18,11 @@ import org.testng.annotations.Test;
 public class AppTest {
 
     private static final String DRIVER_PATH = "C:\\Program Files\\geckodriver\\";
-    private static final String EMAIL = "alinabuzhynskaya@gmail.com";
+    private static final String EMAIL = "annie505smith@gmail.com";
+    private static final String PASSWORD = "annsmith123";
+    private static final String EMAIL2 = "firstlasttest838@gmail.com";
+    private static final String PASSWORD2 = "firstlasttest8381";
+    private static final String TEXTOFEMAIL = "Hello! This is my first test email!";
     public WebDriver driver;
 
     @Test
@@ -21,8 +30,41 @@ public class AppTest {
         System.out.println("launching firefox browser");
         System.setProperty("webdriver.gecko.driver", DRIVER_PATH + "geckodriver.exe");
         driver = new FirefoxDriver();
+        WebDriverWait wait = new WebDriverWait(driver, 10);
         driver.navigate().to("http://www.gmail.com");
+
         driver.findElement(By.id("Email")).sendKeys(EMAIL);
-        //driver.findElement(By.className("gmail-nav__nav-link__sign-in")).click();
+        driver.findElement(By.id("next")).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("Passwd")));
+        driver.findElement(By.id("Passwd")).sendKeys(PASSWORD);
+        driver.findElement(By.id("signIn")).click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[text()='НАПИСАТЬ']")));
+        driver.findElement(By.xpath("//div[text()='НАПИСАТЬ']")).click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("to")));
+        driver.findElement(By.name("to")).sendKeys(EMAIL2);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("subjectbox")));
+        driver.findElement(By.name("subjectbox")).sendKeys("new test");
+//        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(":9f")));
+//        driver.findElement(By.id(":9f")).sendKeys(TEXTOFEMAIL);
+        driver.findElement(By.xpath("//div[text()='Отправить']")).click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("link_vsm")));
+
+        driver.findElement(By.className("gbii")).click();
+        driver.findElement(By.id("gb_71")).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("account-chooser-link")));
+        driver.findElement(By.id("account-chooser-link")).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("account-chooser-add-account")));
+        driver.findElement(By.id("account-chooser-add-account")).click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("Email")));
+        driver.findElement(By.id("Email")).sendKeys(EMAIL2);
+        driver.findElement(By.id("next")).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("Passwd")));
+        driver.findElement(By.id("Passwd")).sendKeys(PASSWORD2);
+        driver.findElement(By.id("signIn")).click();
+
     }
 }
